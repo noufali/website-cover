49455d0c0ff2bc76f990e83571f4993d042dc345
Nouf Aljowaysir <na1215@nyu.edu>
2018-11-06 10:57:05 -0500

    saving to github

--- dance.js
+++ dance.js
@@ -8,11 +8,16 @@
 
 // global threejs variables
 let container, renderer, camera, scene;
+let button, action;
+let clicked = false;
 let controls, loader, effect;
 let pointLight, pointLight2;
 let circles = [];
+let mixers = [];
+let clock = new THREE.Clock();
 
 window.addEventListener('load', onLoad);
+animate();
 
 function onLoad(){
 	container = document.querySelector('#sketch');
@@ -23,103 +28,38 @@ function onLoad(){
 	renderer = new THREE.WebGLRenderer({ });
 	renderer.setPixelRatio(window.devicePixelRatio);
 	renderer.setSize(wid, hei);
+	renderer.shadowMap.enabled = true;
 	container.appendChild(renderer.domElement);
 	scene = new THREE.Scene();
 	scene.background = new THREE.Color( 0x222222 );
-	camera = new THREE.PerspectiveCamera(120, wid/hei, 0.1, 1000);
+	camera = new THREE.PerspectiveCamera(60, wid/hei, 0.1, 2000);
+	// dollyCam = new THREE.PerspectiveCamera();
+	// dollyCam.add(camera);
+	// scene.add(dollyCam);
 
-	dollyCam = new THREE.PerspectiveCamera();
-	dollyCam.add(camera);
-	scene.add(dollyCam);
+	camera.position.set(0, 0, 230);
+	//camera.lookAt(new THREE.Vector3(0,0,50));
+	//camera.target.set(0,0,0);
 
-	dollyCam.position.set(0, -200, 450);
-	//dollyCam.target.set(0, -10, 0);
-	//camera.target.set(0,-10,0);
-
-	effect = new THREE.VREffect(renderer);
-  effect.setSize(wid, hei);
-
-	controls = new THREE.VRControls( camera );
-  controls.standing = true;
-  camera.position.y = controls.userHeight;
+	controls = new THREE.OrbitControls( camera );
+	controls.target.set( 0, 0, 0 );
+	controls.minDistance = 0;
+	controls.maxDistance = 400;
+	controls.minPolarAngle = 0; // radians
+  controls.maxPolarAngle = Math.PI; // radians
+	controls.zoomSpeed = 2.0;
 	controls.update();
 
-	loader = new THREE.TextureLoader();
 	createEnvironment();
-
-	// Initialize (Web)VR
-  renderer.vr.enabled = true;
-  setupVRStage();
+	button = document.getElementById("dance");
 
 	window.addEventListener('resize', onWindowResize, true );
-	window.addEventListener('vrdisplaypresentchange', onWindowResize, true);
 }
-
-// sets up the VR stage + button
-function setupVRStage(){
-  // get available displays
-  navigator.getVRDisplays().then( function(displays){
-    if(displays.length > 0) {
-			// console.log(displays);
-      vrDisplay = displays[0];
-      // setup button
-      vrButton = WEBVR.getButton( vrDisplay, renderer.domElement );
-      document.getElementById('vr_button').appendChild( vrButton );
-    } else {
-      console.log("NO VR DISPLAYS PRESENT");
-    }
-    update();
-  });
-}
-
-// EVENTS
-function onWindowResize(){
-  let wid = window.innerWidth;
-  let hei = window.innerHeight;
-
-	effect.setSize(wid, hei);
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(wid, hei);
-	camera.aspect = wid/hei;
-  camera.updateProjectionMatrix();
-}
-
-
-// ANIMATION
-function update(){
-	window.requestAnimationFrame(animate);
-}
-function animate(timestamp) {
-	let time = performance.now() * 0.001;
-	pointLight.position.x = Math.sin( time * 0.6 ) * 350;
-	pointLight.position.y = Math.sin( time * 0.7 ) * 350 + 5;
-	pointLight.position.z = Math.sin( time * 0.8 ) * 350;
-	pointLight.rotation.x = time;
-	pointLight.rotation.z = time;
-	time += 10000;
-
-	pointLight2.position.x = Math.sin( time * 0.6 ) * 350;
-	pointLight2.position.y = Math.sin( time * 0.7 ) * 350 + 5;
-	pointLight2.position.z = Math.sin( time * 0.8 ) * 350;
-	pointLight2.rotation.x = time;
-	pointLight2.rotation.z = time;
-
-  if(vrDisplay.isPresenting){ // VR rendering
-    controls.update();
-    effect.render(scene, camera);
-    vrDisplay.requestAnimationFrame(animate);
-  } else {  // browser rendering
-		controls.update();
-    renderer.render(scene, camera);
-    window.requestAnimationFrame(animate);
-  }
-}
-
 
 // ENVIRONMENT
 function createEnvironment(){
 	// Room
-	let geometry = new THREE.BoxGeometry( 1000, 1000, 1000 );
+	let geometry = new THREE.BoxGeometry( 400, 400, 800 );
 	let boxMaterial = new THREE.MeshPhongMaterial( {
 					color: 0xa0adaf,
 					shininess: 10,
@@ -128,14 +68,15 @@ function createEnvironment(){
 				} );
 	let cube = new THREE.Mesh( geometry, boxMaterial );
 	cube.receiveShadow = true;
+	cube.position.set(0,0,0);
 	scene.add( cube );
 
 	// Objects in scene
-	let coneGeo = new THREE.ConeGeometry( 300, 1000, 300 );
+	let coneGeo = new THREE.ConeGeometry( 100, 300, 300 );
 	let conMaterial = new THREE.MeshPhongMaterial( {color: 0xffffff} );
 	let cone = new THREE.Mesh( coneGeo, conMaterial );
-	cone.position.set(0,-490,-490);
-	scene.add( cone );
+	cone.position.set(0,0,-380);
+	//scene.add( cone );
 
 	let circle = new THREE.SphereGeometry( 50, 50, 50 );
 	let circMaterial = new THREE.MeshPhongMaterial( {color: 0xffffff} );
@@ -150,7 +91,30 @@ function createEnvironment(){
 	}
 	//circleMesh.position.set(-400,-450,-300);
 
+	//GORILLA
 
+	// model
+	var manager = new THREE.LoadingManager();
+	var textureLoader = new THREE.TextureLoader( manager );
+	var texture = textureLoader.load( 'models/Nouf1_texture.jpg' );
+	var loader = new THREE.FBXLoader();
+	loader.load( 'models/Samba Dancing.fbx', function ( object ) {
+		object.mixer = new THREE.AnimationMixer( object );
+		mixers.push( object.mixer );
+		action = object.mixer.clipAction( object.animations[ 0 ] );
+		action.play();
+		object.traverse( function ( child ) {
+			if ( child.isMesh ) {
+				//child.material.map = texture;
+				child.castShadow = true;
+				child.receiveShadow = true;
+			}
+		} );
+		scene.add( object );
+		//object.scale.set(6, 6, 6)
+	  object.position.set(0,-150,-100);
+	} );
+	console.log(loader);
 
 	// LIGHTS!
 	let ambient = new THREE.AmbientLight( 0x3BAB60,0.5);
@@ -190,6 +154,70 @@ function createEnvironment(){
 
 	scene.add(pointLight2);
 
+}
+
+function dancing() {
+	if (clicked == false) {
+		clicked = true;
+		button.textContent="STOP";
+	} else {
+		clicked = false;
+		button.textContent="DANCE";
+	}
+}
+
+// EVENTS
+function onWindowResize(){
+  let wid = window.innerWidth;
+  let hei = window.innerHeight;
+
+	//effect.setSize(wid, hei);
+  //renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(wid, hei);
+	camera.aspect = wid/hei;
+  camera.updateProjectionMatrix();
+}
 
 
+// ANIMATION
+// function update(){
+// 	window.requestAnimationFrame(animate);
+// }
+
+function animate() {
+
+	window.requestAnimationFrame(animate);
+	let time = performance.now() * 0.001;
+
+	if (clicked == false)
+	{
+		//stay still
+	} else {
+		if ( mixers.length > 0 ) {
+			for ( var i = 0; i < mixers.length; i ++ ) {
+				mixers[ i ].update( clock.getDelta() );
+			}
+		}
+	}
+	// if ( mixers.length > 0 ) {
+	// 	for ( var i = 0; i < mixers.length; i ++ ) {
+	// 		mixers[ i ].update( clock.getDelta() );
+	// 	}
+	// }
+
+	pointLight.position.x = Math.sin( time * 0.6 ) * 400;
+	pointLight.position.y = Math.sin( time * 0.7 ) * 400 + 5;
+	pointLight.position.z = Math.sin( time * 0.8 ) * 400;
+	pointLight.rotation.x = time;
+	pointLight.rotation.z = time;
+	time += 10000;
+
+	// pointLight2.position.x = Math.sin( time * 0.6 ) * 200;
+	// pointLight2.position.y = Math.sin( time * 0.7 ) * 200 + 5;
+	// pointLight2.position.z = Math.sin( time * 0.8 ) * 200;
+	// pointLight2.rotation.x = time;
+	// pointLight2.rotation.z = time;
+
+	controls.update();
+	renderer.render(scene, camera);
 }
